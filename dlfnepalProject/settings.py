1c9d8d78dec6951305dccda26cdf8e5933273697
Sunil <nepasunil@gmail.com>
2021-10-23 18:36:59 +0545

    commit

--- dlfnepalProject/settings.py
+++ dlfnepalProject/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = "django-insecure-m_+z%qm!2_32ktk+ugbj+3!0l37p%6**vrcbv=9ew827-f6)d8"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["dlfnepal.herokuapp.com","localhost"]
+ALLOWED_HOSTS = ["dlfnepal.herokuapp.com","localhost","127.0.0.0"]
 
 
 # Application definition
